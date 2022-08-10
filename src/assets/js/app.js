de940e3d5125fb13aed7630f05a23a3eeebc3069
Marat Minchik <devminrat@gmail.com>
2022-08-10 09:45:23 +0300

    added intro,video,whats sections

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   //= ../../../node_modules/swiper/swiper-bundle.js
   //= components/
+
+  const header = document.querySelector(".header");
+  let scrollPrev = 0;
+
+  window.addEventListener("scroll", () => {
+    let scrolled = document.documentElement.scrollTop;
+
+    if (scrolled > 50 && scrolled > scrollPrev) {
+      header.classList.add("out");
+    } else {
+      header.classList.remove("out");
+    }
+
+    if (scrolled <= 20) {
+      header.classList.add("top");
+    } else {
+      header.classList.remove("top");
+    }
+
+    scrollPrev = scrolled;
+  });
 });
---
+++ src/assets/js/components/sliders.js
@@ -0,0 +1,21 @@
+let videoMainSlider = new Swiper(".video-main__slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  effect: "fade",
+  crossFade: true,
+  navigation: {
+    nextEl: ".slider-btn-next",
+    prevEl: ".slider-btn-prev",
+  },
+  //   breakpoints: {
+  //     320: {
+  //       spaceBetween: 15,
+  //     },
+  //     801: {
+  //       spaceBetween: 10,
+  //     },
+  //     1301: {
+  //       spaceBetween: 20,
+  //     },
+  //   },
+});
