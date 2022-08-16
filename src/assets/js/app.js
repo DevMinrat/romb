09829f1e46aead03be8bcb2ec85976d3f6cd5bc6
Marat Minchik <devminrat@gmail.com>
2022-08-16 18:18:09 +0300

    added launch timer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,12 @@
+//= components/jquery3.6.0.js
+
 document.addEventListener("DOMContentLoaded", () => {
   //= ../../../node_modules/swiper/swiper-bundle.js
-  //= ../../../node_modules/threesixty-slider/dist/threesixty.min.js
   //= ../../../node_modules/chart.js/dist/chart.js
-  //= components/
+  //= components/threesixty.js
+  //= components/sliders.js
+  //= components/charts.js
+  //= components/threesixty-sliders.js
 
   const header = document.querySelector(".header");
   let scrollPrev = 0;
@@ -36,9 +40,69 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   if (chooseItems.length > 0) {
-    let firstItemBars = chooseItems[0].querySelectorAll(".ci-data__item-progress");
+    let firstItemBars = chooseItems[0].querySelectorAll(
+      ".ci-data__item-progress"
+    );
     firstItemBars.forEach((bar) => {
       animateBars(bar);
     });
+  }
+
+  // launching timer
+
+  const launchingTimer = document.querySelector(".launching-timer");
+
+  if (launchingTimer) {
+    const deadline = launchingTimer.dataset.timer;
+
+    function getTimeRemaining(endtime) {
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+        minuts = Math.floor((t / 1000 / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+
+      return {
+        total: t,
+        days: days,
+        hours: hours,
+        minutes: minuts,
+        seconds: seconds,
+      };
+    }
+
+    function getZero(num) {
+      if (num >= 0 && num < 10) {
+        return `0${num}`;
+      } else {
+        return num;
+      }
+    }
+
+    function setClock(selector, endtime) {
+      const timer = document.querySelector(selector),
+        days = timer.querySelector("#launching-timer__days"),
+        hours = timer.querySelector("#launching-timer__hours"),
+        minutes = timer.querySelector("#launching-timer__minutes"),
+        seconds = timer.querySelector("#launching-timer__seconds"),
+        timeInterval = setInterval(updateClock, 1000);
+
+      updateClock();
+
+      function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {
+          clearInterval(timeInterval);
+        }
+      }
+    }
+
+    setClock(".launching-timer__content", deadline);
   }
 });
--- src/assets/js/components/threesixty-sliders.js
+++ src/assets/js/components/threesixty-sliders.js
@@ -1,77 +1,117 @@
-$(document).ready(function () {
-  var three60Uniform = $(".uniform").ThreeSixty({
-    totalFrames: 36,
-    endFrame: 36,
-    currentFrame: 1,
-    imgList: ".threesixty_images",
-    progress: ".spinner",
-    imagePath: "assets/images/equipment/uniform/small/",
-    filePrefix: "",
-    ext: ".png",
-    height: 250,
-    width: 250,
-    navigation: false,
-    disableSpin: false,
-    responsive: true,
-  });
-  var three60Helmet = $(".helmet").ThreeSixty({
-    totalFrames: 36,
-    endFrame: 36,
-    currentFrame: 1,
-    imgList: ".threesixty_images",
-    progress: ".spinner",
-    imagePath: "assets/images/equipment/uniform/small/",
-    filePrefix: "",
-    ext: ".png",
-    height: 250,
-    width: 250,
-    navigation: false,
-    disableSpin: false,
-    responsive: true,
-  });
-  var three60Gloves = $(".gloves").ThreeSixty({
-    totalFrames: 36,
-    endFrame: 36,
-    currentFrame: 1,
-    imgList: ".threesixty_images",
-    progress: ".spinner",
-    imagePath: "assets/images/equipment/uniform/small/",
-    filePrefix: "",
-    ext: ".png",
-    height: 250,
-    width: 250,
-    navigation: false,
-    disableSpin: false,
-    responsive: true,
-  });
-  var three60Mouthguard = $(".mouthguard").ThreeSixty({
-    totalFrames: 36,
-    endFrame: 36,
-    currentFrame: 1,
-    imgList: ".threesixty_images",
-    progress: ".spinner",
-    imagePath: "assets/images/equipment/uniform/small/",
-    filePrefix: "",
-    ext: ".png",
-    height: 250,
-    width: 250,
-    navigation: false,
-    disableSpin: false,
-    responsive: true,
-  });
-  var three60Skillsighter = $(".skills-fighter").ThreeSixty({
-    totalFrames: 36,
-    endFrame: 36,
-    currentFrame: 1,
-    imgList: ".threesixty_images",
-    progress: ".spinner",
-    imagePath: "assets/images/skills/fighter/",
-    filePrefix: "sf-",
-    ext: ".png",
-    height: 810,
-    width: 810,
-    navigation: false,
-    disableSpin: false,
-    responsive: true,
+$(window).on("load", function () {
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return elemBottom <= docViewBottom && elemTop >= docViewTop;
+  }
+
+  function initThreeSixtyUniform() {
+    var three60Uniform = $(".uniform").ThreeSixty({
+      totalFrames: 36,
+      endFrame: 36,
+      currentFrame: 1,
+      imgList: ".threesixty_images",
+      progress: ".spinner",
+      imagePath: "assets/images/equipment/uniform/small/",
+      filePrefix: "",
+      ext: ".png",
+      height: 250,
+      width: 250,
+      navigation: false,
+      disableSpin: false,
+      responsive: true,
+    });
+    var three60Helmet = $(".helmet").ThreeSixty({
+      totalFrames: 36,
+      endFrame: 36,
+      currentFrame: 1,
+      imgList: ".threesixty_images",
+      progress: ".spinner",
+      imagePath: "assets/images/equipment/uniform/small/",
+      filePrefix: "",
+      ext: ".png",
+      height: 250,
+      width: 250,
+      navigation: false,
+      disableSpin: false,
+      responsive: true,
+    });
+    var three60Gloves = $(".gloves").ThreeSixty({
+      totalFrames: 36,
+      endFrame: 36,
+      currentFrame: 1,
+      imgList: ".threesixty_images",
+      progress: ".spinner",
+      imagePath: "assets/images/equipment/uniform/small/",
+      filePrefix: "",
+      ext: ".png",
+      height: 250,
+      width: 250,
+      navigation: false,
+      disableSpin: false,
+      responsive: true,
+    });
+    var three60Mouthguard = $(".mouthguard").ThreeSixty({
+      totalFrames: 36,
+      endFrame: 36,
+      currentFrame: 1,
+      imgList: ".threesixty_images",
+      progress: ".spinner",
+      imagePath: "assets/images/equipment/uniform/small/",
+      filePrefix: "",
+      ext: ".png",
+      height: 250,
+      width: 250,
+      navigation: false,
+      disableSpin: false,
+      responsive: true,
+    });
+
+    $(".equip-content").removeClass("hidden");
+  }
+
+  function initThreeSixtySkillFighter() {
+    var three60SkillsFighter = $(".skills-fighter").ThreeSixty({
+      totalFrames: 36,
+      endFrame: 36,
+      currentFrame: 1,
+      imgList: ".threesixty_images",
+      progress: ".spinner",
+      imagePath: "assets/images/skills/fighter/",
+      filePrefix: "sf-",
+      ext: ".png",
+      height: 810,
+      width: 810,
+      navigation: false,
+      disableSpin: false,
+      responsive: true,
+    });
+    $(".skills-slider").removeClass("hidden");
+  }
+
+  if (isScrolledIntoView($(".equip-content"))) {
+    initThreeSixtyUniform();
+  }
+  if (isScrolledIntoView($(".skills-slider"))) {
+    initThreeSixtySkillFighter();
+  }
+
+  window.addEventListener("scroll", () => {
+    if (
+      isScrolledIntoView($(".equip-content")) &&
+      $(".equip-content").hasClass("hidden")
+    ) {
+      initThreeSixtyUniform();
+    }
+    if (
+      isScrolledIntoView($(".skills-slider")) &&
+      $(".skills-slider").hasClass("hidden")
+    ) {
+      initThreeSixtySkillFighter();
+    }
   });
 });
